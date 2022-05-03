5ffcb8dc70630993139b49dd0f53dbc596f37594
mitchsteamy <91216056+mitchsteamy@users.noreply.github.com>
2022-05-03 19:35:15 -0400

    Update config.py

--- components/config.py
+++ components/config.py
@@ -12,27 +12,29 @@ class PongConfig:
     line_weight: int
 
 config = PongConfig(
-    white=(255, 255, 255),
-    black=(0, 0, 0),
-    red=(200, 56, 49),
-    blue=(65, 105, 225),
-    height=800,
-    width=1000,
-    line_weight=5,
+    white = (255, 255, 255),
+    black = (0, 0, 0),
+    red = (200, 56, 49),
+    blue = (65, 105, 225),
+    height = 800,
+    width = 1000,
+    line_weight = 5,
 )
 
 @dataclass
 class PongFlags:
     running: bool
+    setup_mode: bool
     playing: bool
-    multiplayer: bool
     game_over: bool
+    multiplayer: bool
     difficulty: str
 
 flags = PongFlags(
-    running=True,
-    playing=False,
-    multiplayer=False,
-    game_over=False,
-    difficulty=None,
+    running = True,
+    setup_mode = True,
+    playing = False,
+    game_over = False,
+    multiplayer = False,
+    difficulty = None,
 )
